7a7531463f62b14058b34029a937305c144e0a70
David Bailey <github-mycareer@dflatdeveloper.net>
2024-11-01 18:33:14 -0400

    Remove boilerplate code

--- MessagingExample/messagingexample.client/src/main.ts
+++ MessagingExample/messagingexample.client/src/main.ts
@@ -1,10 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 import { PayloadModule } from './payload/payload.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
 
 platformBrowserDynamic().bootstrapModule(PayloadModule)
   .catch(err => console.error(err));
